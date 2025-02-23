13d8d278ea52f0ee72e9cd86ccdb9dd64b7aa66b
pavani <muthyalapavanireddy1997@gmail.com>
2025-02-23 23:13:42 +0530

    23 Feb 2025

---
+++ src/ex_15_OOPs/Lab157.java
@@ -0,0 +1,15 @@
+package ex_15_OOPs;
+
+public class Lab157 {
+    public static void main(String[] args) {
+
+     ATBx amit =new ATBx();
+      amit.name= "Amit Sharma";
+      amit.salary =3000;
+     ATBx pramod =new ATBx();
+     pramod.name="pramod Dutta";
+     pramod.salary =2000;
+
+
+    }
+}
---
+++ src/ex_15_OOPs/Lab_158_ATBx.java
@@ -0,0 +1,22 @@
+package ex_15_OOPs;
+
+public class Lab_158_ATBx {
+    public static void main(String[] args) {
+
+        //Attribute
+        String name;
+        int age;
+        String gender;
+        int salary;
+        long phone_no;
+        String address;
+
+
+        //behaviour
+
+    //  void speak(){}
+//        void eat(){}
+//        void sleep(){};
+
+    }
+}
