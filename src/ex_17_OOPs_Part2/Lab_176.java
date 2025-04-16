de9e1be9cccf811331d9dcfe1c9ff084610d1202
pavani <muthyalapavanireddy1997@gmail.com>
2025-04-16 16:50:02 +0530

    16 april 2025

--- src/ex_17_OOPs_Part2/Lab_176.java
+++ src/ex_17_OOPs_Part2/Lab_176.java
@@ -5,6 +5,6 @@ import ex_17_OOPs_Part2.police.cop;
 public class Lab_176 {
     public static void main(String[] args) {
         cop c1 =new cop(10);
-        c1.canIShoot();
+       // c1.canIShoot();
     }
 }
