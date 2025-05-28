be1fc86898c76658852de7839eca4ac84ec1d534
pavani <muthyalapavanireddy1997@gmail.com>
2025-05-28 23:29:43 +0530

    28th may 2025

---
+++ src/ex_25_Collections_Framework/Lab_218_List.java
@@ -0,0 +1,41 @@
+package ex_25_Collections_Framework;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab_218_List {
+    public static void main(String[] args) {
+//
+//     List l= List.of("orange","apple","banana","mango","watermelon");
+//        System.out.println(l);
+     //static method can be fully implemented in terface.
+       // default method - can full implemented in interface
+
+     //l.add("chery"); // unsportedOperatiponsException
+     //   l.remove("apple"); //not possible
+
+
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("pavani");
+        arrayList.add("reddy");
+        arrayList.add("null");
+
+        arrayList.add("reddy"); // duplicate are allowed
+        arrayList.add(123); // different datatype are allowed
+
+        System.out.println(arrayList);
+
+        System.out.println(arrayList.size());
+
+
+
+        List l = new ArrayList(); //Dyanmic dispatch
+        l.add("123");
+        l.add("456");
+        System.out.println(l);
+
+
+
+    }
+}
---
+++ src/ex_25_Collections_Framework/Lab_219_Arraylist.java
@@ -0,0 +1,31 @@
+package ex_25_Collections_Framework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Lab_219_Arraylist {
+    public static void main(String[] args) {
+
+
+        List list = new ArrayList();
+        list.add("1"); //0
+        list.add("2"); //1
+        list.add("3");  //2
+        list.add("4"); //3
+        list.add(5);
+
+        System.out.println(list.size());
+        System.out.println(list.isEmpty());
+        System.out.println(list.contains("1"));
+        System.out.println(list.contains(1)); // interger it is ,all of them are string ""
+        System.out.println(list.indexOf("1"));
+        System.out.println(list.lastIndexOf("4"));
+
+        System.out.println(list); // to print all
+        for(in i)
+
+
+
+    }
+}
